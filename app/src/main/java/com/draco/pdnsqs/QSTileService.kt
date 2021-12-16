351f53f7dc8ef09702d39c8fcb8e63e507df0043
Tyler Nijmeh <tylernij@gmail.com>
2021-12-15 23:29:25 -0800

    Copy button for command
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- app/src/main/java/com/draco/pdnsqs/QSTileService.kt
+++ app/src/main/java/com/draco/pdnsqs/QSTileService.kt
@@ -1,22 +1,39 @@
 package com.draco.pdnsqs
 
 import android.app.AlertDialog
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
 import android.content.Intent
 import android.os.IBinder
 import android.service.quicksettings.Tile
 import android.service.quicksettings.TileService
 
 class QSTileService : TileService() {
+    companion object {
+        const val ADB_COMMAND = "adb shell pm grant ${BuildConfig.APPLICATION_ID} android.permission.WRITE_SECURE_SETTINGS"
+    }
+
     private lateinit var secureSettings: SecureSettings
     private lateinit var dialog: AlertDialog
 
+    private lateinit var clipboardManager: ClipboardManager
+
     override fun onBind(intent: Intent?): IBinder? {
         secureSettings = SecureSettings(applicationContext)
+        clipboardManager = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
 
         dialog = AlertDialog.Builder(applicationContext)
             .setTitle(R.string.permission_title)
             .setMessage(R.string.permission_message)
             .setPositiveButton(R.string.permission_dismiss) { _, _ -> }
+            .setNeutralButton(R.string.permission_copy) { _, _ ->
+                val clipData = ClipData.newPlainText(
+                    getString(R.string.permission_title),
+                    ADB_COMMAND
+                )
+                clipboardManager.setPrimaryClip(clipData)
+            }
             .create()
 
         return super.onBind(intent)
